15aaeb82d5c0886a90df5af74307926fa5e34e29
Busra Ozturk <bsra.6141@outlook.com>
2025-07-14 00:39:56 +0300

    fix fix

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*", // Geliştirme için açık, prod da domain sınırla
-    methods: ["GET", "POST"],
-  },
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
+
+app.use(express.static(path.join(__dirname, "../client")));
 
 const rooms = {};
 
@@ -20,42 +23,30 @@ io.on("connection", (socket) => {
   socket.on("join-room", ({ roomId, username }) => {
     socket.join(roomId);
 
-    if (!rooms[roomId]) rooms[roomId] = [];
-
-    // Eğer kullanıcı zaten varsa tekrar eklemeyelim
-    if (!rooms[roomId].some(u => u.id === socket.id)) {
-      rooms[roomId].push({ id: socket.id, name: username });
+    if (!rooms[roomId]) {
+      rooms[roomId] = { screenSharer: null, users: [] };
     }
 
-    // Kullanıcı listesi gönder
-    io.to(roomId).emit("update-user-list", rooms[roomId]);
+    rooms[roomId].users.push({ id: socket.id, name: username });
 
-    // Diğer kullanıcılara haber ver
+    io.to(roomId).emit("update-user-list", rooms[roomId].users);
+
     socket.to(roomId).emit("user-joined", socket.id);
 
-    // Ekran paylaşma eventleri
     socket.on("start-screen-share", () => {
-      // O odadaki tüm diğer ekran paylaşanları durdur
-      const currentSharer = rooms[roomId].find(u => u.isScreenSharing);
-      if (currentSharer && currentSharer.id !== socket.id) {
-        // Önceki ekran paylaşan bilgisini temizle
-        currentSharer.isScreenSharing = false;
-        io.to(roomId).emit("screen-share-stopped", currentSharer.id);
+      const currentSharer = rooms[roomId].screenSharer;
+      if (currentSharer && currentSharer !== socket.id) {
+        io.to(currentSharer).emit("stop-screen-share");
       }
-      // Bu kullanıcıyı ekran paylaşan yap
-      const user = rooms[roomId].find(u => u.id === socket.id);
-      if (user) {
-        user.isScreenSharing = true;
-      }
-      io.to(roomId).emit("screen-share-started", socket.id);
+      rooms[roomId].screenSharer = socket.id;
+      socket.to(roomId).emit("screen-share-started", socket.id);
     });
 
     socket.on("stop-screen-share", () => {
-      const user = rooms[roomId].find(u => u.id === socket.id);
-      if (user) {
-        user.isScreenSharing = false;
+      if (rooms[roomId].screenSharer === socket.id) {
+        rooms[roomId].screenSharer = null;
+        socket.to(roomId).emit("screen-share-stopped", socket.id);
       }
-      io.to(roomId).emit("screen-share-stopped");
     });
 
     socket.on("signal", (data) => {
@@ -65,22 +56,22 @@ io.on("connection", (socket) => {
       });
     });
 
-    socket.on("disconnect", () => {
-      // Kullanıcıyı odadan çıkar
+    socket.on("request-user-list-update", () => {
       if (rooms[roomId]) {
-        rooms[roomId] = rooms[roomId].filter((u) => u.id !== socket.id);
-
-        // Eğer ekran paylaşanıysa yayın durdurulsun
-        io.to(roomId).emit("screen-share-stopped", socket.id);
-
-        io.to(roomId).emit("update-user-list", rooms[roomId]);
-        socket.to(roomId).emit("user-left", socket.id);
-
-        // Temizle boşsa oda
-        if (rooms[roomId].length === 0) {
-          delete rooms[roomId];
-        }
+        io.to(roomId).emit("update-user-list", rooms[roomId].users);
       }
+    });
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-left", socket.id);
+      if (!rooms[roomId]) return;
+
+      rooms[roomId].users = rooms[roomId].users.filter(u => u.id !== socket.id);
+      if (rooms[roomId].screenSharer === socket.id) {
+        rooms[roomId].screenSharer = null;
+        socket.to(roomId).emit("screen-share-stopped", socket.id);
+      }
+      io.to(roomId).emit("update-user-list", rooms[roomId].users);
     });
   });
 });
